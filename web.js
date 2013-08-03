b79d2722bdf755efc44a038dd067ec4cc201fa89
Ubuntu <ubuntu@ip-172-31-41-4.us-west-2.compute.internal>
2013-08-03 04:57:47 +0000

    bootstrap testing carousel2

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var fs = require('fs');
-var htmlfile = "/home/ubuntu/bitstarter/bootstrap-examples-master/carousel/index.html";
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
