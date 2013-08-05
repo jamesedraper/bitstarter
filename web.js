2f21e90ac8852afe1ceea57aac07df726745756a
Ubuntu <ubuntu@ip-172-31-41-4.us-west-2.compute.internal>
2013-08-05 23:28:11 +0000

    Testing routing

--- web.js
+++ web.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var fs = require('fs');
-var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var html = fs.readFileSync(htmlfile).toString();
+  var htmlfile = "index.html";
+  response.send(html);
+});
+
+app.get('/contact', function(request, response) {
+  var html = 'Contact Page';
   response.send(html);
 });
 
